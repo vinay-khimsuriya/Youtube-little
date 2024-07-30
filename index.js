4b48931d4166a85194cf2273e7dbe3b251f65cff
vinay khimauriya <khimsuriyavinay29@yahoo.com>
2024-07-30 11:57:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import App from './FrontEnd/src/App'
+import './App.css'
+import ReactDOM from 'react-dom'
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>)
